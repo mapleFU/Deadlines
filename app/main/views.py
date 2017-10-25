fcc53f82aa0d791e2ec58dac2c64ec1ef59cb702
付旭炜 <1506118561@qq.com>
2017-10-26 01:38:50 +0800

    add paging for the program

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from . import main_blueprint
-from flask import render_template, abort, redirect, url_for
+from flask import render_template, abort, redirect, url_for, request
 from flask_login import login_required, current_user
 from app.model import User, Task
 from .forms import TaskForm
@@ -27,8 +27,14 @@ def index():
         db.session.commit()
         # maybe i should clear it?
         return redirect(url_for('.index'))
-    tasks = Task.query.order_by(Task.ending).all()
+    # attention
+    page = request.args.get('page', 1, type=int)
+    pag = Task.query.order_by(Task.ending).paginate(
+        page, per_page=10, error_out=False
+    )
+    # tasks = Task.query.order_by(Task.ending).all()
     # print(tasks)
+    tasks = pag.items
     return render_template('index.html', tsks=tasks, form=form)
 
 
